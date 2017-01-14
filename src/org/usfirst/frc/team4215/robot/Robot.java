bab73cce7f64aa2e82f828f8c1dcce9a73b8531a
Unknown <carl.yaeger.phd@gmail.com>
2017-01-14 09:55:33 -0600

    first commit
    #1

---
+++ src/org/usfirst/frc/team4215/robot/Robot.java
@@ -0,0 +1,123 @@
+package org.usfirst.frc.team4215.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	final String defaultAuto = "Default";
+	final String customAuto = "My Auto";
+	String autoSelected;
+	SendableChooser<String> chooser = new SendableChooser<>();
+	//23-26 declare Victors
+	Victor flWheel = new Victor(3);
+	Victor frWheel = new Victor(0);
+	Victor blWheel = new Victor(1);
+	Victor brWheel = new Victor(2);
+	//28 declares joystick
+	Joystick driveJoystick = new Joystick(1);
+	//30-31 declare arm Victors (which I think will always run together)
+	Victor arm1 = new Victor(4);
+	Victor arm2 = new Victor(6);
+	//33 declares arm joystick
+	Joystick armJoystick = new Joystick(2);
+	
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		chooser.addDefault("Default Auto", defaultAuto);
+		chooser.addObject("My Auto", customAuto);
+		SmartDashboard.putData("Auto choices", chooser);
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+		autoSelected = chooser.getSelected();
+		// autoSelected = SmartDashboard.getString("Auto Selector",
+		// defaultAuto);
+		System.out.println("Auto selected: " + autoSelected);
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		switch (autoSelected) {
+		case customAuto:
+			// Put custom auto code here
+			break;
+		case defaultAuto:
+		default:
+			// Put default auto code here
+			break;
+		}
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+		//86-87 Gets position of left joystick and sets it to wheelPower
+		GenericHID.Hand leftJoystick = GenericHID.Hand.kLeft;
+		double wheelPower = driveJoystick.getY(leftJoystick);
+		
+		//91-94 sets min/max wheel power to -1 and 1
+		if (wheelPower > 1) {
+			wheelPower = 1;
+		} else if (wheelPower < -1) {
+			wheelPower = -1;
+		}
+		//97-100 sets wheels to left joystick position
+		flWheel.set(wheelPower);
+		frWheel.set(wheelPower);
+		blWheel.set(wheelPower);
+		brWheel.set(wheelPower);
+		
+		GenericHID.Hand rightJoystick = GenericHID.Hand.kRight;
+		double armPower = armJoystick.getY(rightJoystick);
+		
+		//106-110 sets min/max arm power to -1 and 1
+		if (armPower > 1) {
+			armPower = 1;
+		} else if (armPower < -1) {
+			armPower = -1;
+		}
+		//112-113 sets arm motors to right joystick position
+		arm1.set(armPower);
+		arm2.set(armPower);
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+}
+
